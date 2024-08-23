10f112fe49b759662971ff8f823774b2ac1cef8d
Ryosuke Yasuoka <ryasuoka@redhat.com>
2024-08-23 11:25:18 +0900

    list: Support paused state
    Signed-off-by: Ryosuke Yasuoka <ryasuoka@redhat.com>

--- src/list.rs
+++ src/list.rs
@@ -1,5 +1,19 @@
 use config::Config;
-use virt::connect::Connect;
+use virt::{
+    connect::Connect,
+    domain::Domain,
+    sys::{VIR_DOMAIN_PAUSED, VIR_DOMAIN_RUNNING, VIR_DOMAIN_SHUTOFF},
+};
+
+fn get_state_str(dom: &Domain) -> &str {
+    match dom.get_state().unwrap() {
+        (VIR_DOMAIN_RUNNING, _) => "running",
+        (VIR_DOMAIN_PAUSED, _) => "paused",
+        (VIR_DOMAIN_SHUTOFF, _) => "shut off",
+        // TODO: support other states on demand
+        _ => "-",
+    }
+}
 
 pub fn main(settings: &Config) {
     let uri = settings.get_string("URI").unwrap();
@@ -11,11 +25,7 @@ pub fn main(settings: &Config) {
     println!("{:<20} {}", "--------------------", "--------");
     for dom in doms {
         let name = dom.get_name().unwrap();
-        let state = if dom.is_active().unwrap() {
-            "running"
-        } else {
-            "shut off"
-        };
+        let state = get_state_str(&dom);
 
         println!("{:<20} {}", name, state);
     }
