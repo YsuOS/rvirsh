58118d4945cf5d5fe64b32bee9cdc25e6734824f
Ryosuke Yasuoka <ryasuoka@redhat.com>
2024-08-23 11:30:33 +0900

    suspend: Check if the dom is active
    Signed-off-by: Ryosuke Yasuoka <ryasuoka@redhat.com>

--- src/suspend.rs
+++ src/suspend.rs
@@ -7,6 +7,11 @@ fn show_help() {
 }
 
 fn suspend_domain(dom: &Domain) {
+    if !dom.is_active().unwrap() {
+        println!("Domain {} is inactive", &dom.get_name().unwrap());
+        return;
+    }
+
     dom.suspend().unwrap();
     println!("Domain {} suspended", &dom.get_name().unwrap());
 }
