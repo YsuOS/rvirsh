a42d2ad73fa5d0060dcd97f6f58a53bbfab0f418
Ryosuke Yasuoka <ryasuoka@redhat.com>
2024-08-27 17:24:34 +0900

    snapshot-delete: Add msg
    Signed-off-by: Ryosuke Yasuoka <ryasuoka@redhat.com>

--- src/snapshot/snapshot_delete.rs
+++ src/snapshot/snapshot_delete.rs
@@ -5,6 +5,7 @@ pub fn delete_all_snapshots(dom: &Domain) {
 
     for snapshot in snapshots {
         snapshot.delete(0).unwrap();
+        println!("{} is deleted", snapshot.get_name().unwrap());
     }
     println!("All snapshots of {} is deleted", &dom.get_name().unwrap());
 }
