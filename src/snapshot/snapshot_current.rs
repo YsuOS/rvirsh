69164f3055944186229c4882130d344608fcaecb
Ryosuke Yasuoka <ryasuoka@redhat.com>
2024-09-19 13:33:06 +0900

    snapshot-current: show snapshot name instead of xml
    Signed-off-by: Ryosuke Yasuoka <ryasuoka@redhat.com>

--- src/snapshot/snapshot_current.rs
+++ src/snapshot/snapshot_current.rs
@@ -3,5 +3,5 @@ use virt::{domain::Domain, domain_snapshot::DomainSnapshot};
 pub fn get_current_snapshot(dom: &Domain) {
     let snapshot = DomainSnapshot::current(dom, 0).unwrap();
 
-    crate::snapshot::snapshot_dumpxml::show_snapshot_dumpxml(&snapshot);
+    println!("{}", snapshot.get_name().unwrap());
 }
