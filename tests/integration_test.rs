3d19b3259ba6139f7dcdf475c62fa775288bcb55
Ryosuke Yasuoka <ryasuoka@redhat.com>
2024-09-22 01:49:04 +0900

    Add main test
    Signed-off-by: Ryosuke Yasuoka <ryasuoka@redhat.com>

---
+++ tests/integration_test.rs
@@ -0,0 +1,38 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+
+#[test]
+fn without_command() {
+    Command::cargo_bin("rv")
+        .unwrap()
+        .assert()
+        .failure()
+        .stderr(predicate::eq(
+            "Error: 1st argument is required\nRun 'rv help' to see commands\n",
+        ));
+}
+
+#[test]
+fn unsupported_command() {
+    let cmd = "unsupported";
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg(cmd)
+        .assert()
+        .failure()
+        .stderr(predicate::eq(format!(
+            "Error: Command {} is not supported.\nRun 'rv help' to see commands\n",
+            cmd
+        )));
+}
+
+#[test]
+fn version() {
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("version")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Using Library:"))
+        .stdout(predicate::str::contains("Running hypervisor:"));
+}
