c447f7e5af7d5f0b1c6f556d8e521d3d988d21b9
Ryosuke Yasuoka <ryasuoka@redhat.com>
2024-09-24 17:04:45 +0900

    domid: remove unwrap
    Signed-off-by: Ryosuke Yasuoka <ryasuoka@redhat.com>

--- src/domain/domid.rs
+++ src/domain/domid.rs
@@ -1,7 +1,7 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use virt::domain::Domain;
 
 pub fn show_domain_id(dom: &Domain) -> Result<()> {
-    println!("{}", dom.get_id().unwrap());
+    println!("{}", dom.get_id().context("Can not find ID")?);
     Ok(())
 }
