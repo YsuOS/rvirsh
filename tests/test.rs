1484048c63ba168ec776bc8be16977dfd5fcd107
Ryosuke Yasuoka <ryasuoka@redhat.com>
2024-09-25 00:48:34 +0900

    Add domain test
    Signed-off-by: Ryosuke Yasuoka <ryasuoka@redhat.com>

--- tests/test.rs
+++ tests/test.rs
@@ -57,3 +57,170 @@ fn info_without_dom() {
         .assert()
         .failure();
 }
+
+#[test]
+fn temporary_domain_test() {
+    let xml_path = env!("CARGO_MANIFEST_DIR").to_string() + "/resources/test.xml";
+    let vm_name = "test-vm";
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("run")
+        .arg(&xml_path)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("suspend")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("resume")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("poweroff")
+        .arg(vm_name)
+        .assert()
+        .success();
+}
+
+#[test]
+fn domain_test() {
+    let xml_path = env!("CARGO_MANIFEST_DIR").to_string() + "/resources/test1.xml";
+    let vm_name = "test-vm1";
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("define")
+        .arg(&xml_path)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("start")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("autostart")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("domid")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("dominfo")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("domuuid")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("domstate")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("dumpxml")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("info")
+        .arg(vm_name)
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(format!(
+            "{:<20} {}",
+            "Autostart:", "enable"
+        )));
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("noautostart")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("info")
+        .arg(vm_name)
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(format!(
+            "{:<20} {}",
+            "Autostart:", "disable"
+        )));
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("reset")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("reboot")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("shutdown")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("reset")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("undefine")
+        .arg(vm_name)
+        .assert()
+        .success();
+
+    Command::cargo_bin("rv")
+        .unwrap()
+        .arg("poweroff")
+        .arg(vm_name)
+        .assert()
+        .success();
+}
