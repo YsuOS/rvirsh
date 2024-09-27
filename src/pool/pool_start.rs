3806760e84004e904c2e43b5b1ad2e59fddaafd1
Ryosuke Yasuoka <ryasuoka@redhat.com>
2024-09-27 18:28:47 +0900

    pool-start: it works if pool is not active
    Signed-off-by: Ryosuke Yasuoka <ryasuoka@redhat.com>

--- src/pool/pool_start.rs
+++ src/pool/pool_start.rs
@@ -2,7 +2,7 @@ use anyhow::{bail, Result};
 use virt::{storage_pool::StoragePool, sys::VIR_STORAGE_POOL_CREATE_NORMAL};
 
 pub fn start_pool(pool: &StoragePool) -> Result<()> {
-    if !pool.is_active()? {
+    if pool.is_active()? {
         bail!("{} is already active", pool.get_name()?)
     }
     pool.create(VIR_STORAGE_POOL_CREATE_NORMAL)?;
