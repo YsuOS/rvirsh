6ce3884c9d05c77877b2accf024390a28be6cf80
Ryosuke Yasuoka <ryasuoka@redhat.com>
2024-11-25 02:31:11 +0900

    clone: clean up
    Signed-off-by: Ryosuke Yasuoka <ryasuoka@redhat.com>

--- src/clone.rs
+++ src/clone.rs
@@ -1,5 +1,5 @@
 use crate::{get_args, get_conn, get_domain};
-use anyhow::{anyhow, Context, Result};
+use anyhow::{Context, Result};
 use config::Config;
 use quick_xml::{events::Event, Reader};
 use regex::Regex;
@@ -28,7 +28,7 @@ fn get_volume_from_domain(dom: &Domain) -> Result<StorageVol> {
         }
     }
     let file_path = file_path.with_context(|| {
-        anyhow!(
+        format!(
             "Could not find volume from {}'s xml",
             dom.get_name().unwrap()
         )
@@ -40,34 +40,35 @@ fn get_vol_name(vol_path: &str) -> Result<String> {
     let vol_name = Path::new(vol_path)
         .file_name()
         .and_then(|name| name.to_str())
-        .context(anyhow!("Could not get volume name from volume path"))?;
+        .with_context(|| format!("Could not get volume name from volume path"))?;
     Ok(vol_name.to_string())
 }
 
 fn create_new_volume_xml(org_vol: &StorageVol, new_vol_path: &str) -> Result<String> {
-    let vol_xml = org_vol.get_xml_desc(0)?;
-    let updated_xml = vol_xml
+    let new_vol_xml = org_vol
+        .get_xml_desc(0)?
         .replace(&org_vol.get_path()?, new_vol_path)
         .replace(&org_vol.get_name()?, &get_vol_name(new_vol_path)?);
-    Ok(updated_xml)
+    Ok(new_vol_xml)
 }
 
-fn remove_uuid_lines(xml: &str) -> String {
-    let re = Regex::new(r"(?m)^\s*<uuid>.*</uuid>\s*$").unwrap();
-    re.replace_all(xml, "").to_string()
+fn remove_uuid_lines(xml: &str) -> Result<String> {
+    let re = Regex::new(r"(?m)^\s*<uuid>.*</uuid>\s*$")?;
+    Ok(re.replace_all(xml, "").to_string())
 }
 
 fn create_new_domain_xml(
-    org_xml: &str,
+    org_dom: &Domain,
     org_vol: &StorageVol,
-    new_vol_path: &str,
-    org_name: &str,
     new_name: &str,
+    new_vol_path: &str,
 ) -> Result<String> {
-    let new_xml = org_xml
-        .replace(org_name, new_name)
+    let org_name = org_dom.get_name()?;
+    let new_xml = org_dom
+        .get_xml_desc(0)?
+        .replace(&org_name, new_name)
         .replace(&org_vol.get_path()?, new_vol_path);
-    Ok(remove_uuid_lines(&new_xml))
+    remove_uuid_lines(&new_xml)
 }
 
 fn clone_get_args(index: usize, msg: &str, cmd: &str) -> Result<String> {
@@ -75,41 +76,41 @@ fn clone_get_args(index: usize, msg: &str, cmd: &str) -> Result<String> {
     get_args(index, msg, cmd, &usage)
 }
 
-// `rv clone`
+/// `rv clone`
 pub fn clone_domain(
     conn: &Connect,
-    org_name: &str,
-    org_xml: &str,
-    org_vol: &StorageVol,
+    org_dom: &Domain,
     new_name: &str,
     new_pool: &StoragePool,
     new_vol_path: &str,
 ) -> Result<()> {
-    let new_vol_xml = create_new_volume_xml(org_vol, new_vol_path)?;
-    let new_xml = create_new_domain_xml(org_xml, org_vol, new_vol_path, org_name, new_name)?;
+    let org_vol = get_volume_from_domain(&org_dom)?;
+
+    let new_vol_xml = create_new_volume_xml(&org_vol, new_vol_path)?;
+    let new_xml = create_new_domain_xml(&org_dom, &org_vol, new_name, new_vol_path)?;
 
     crate::pool::refresh_pool(new_pool)?;
-    StorageVol::create_xml_from(new_pool, &new_vol_xml, org_vol, 0)?;
+    StorageVol::create_xml_from(new_pool, &new_vol_xml, &org_vol, 0)?;
 
     let dom = Domain::define_xml(conn, &new_xml)?;
-    println!("Domain {} is cloned from {}", dom.get_name()?, org_name);
+    println!(
+        "Domain {} is cloned from {}",
+        dom.get_name()?,
+        org_dom.get_name()?
+    );
     Ok(())
 }
 
 pub fn main(settings: &Config, cmd: &str) -> Result<()> {
     let conn = get_conn(settings)?;
-    let dom = get_domain(&conn, cmd)?;
 
+    let new_pool = StoragePool::lookup_by_name(&conn, &settings.get_string("POOL")?)?;
     let new_name = clone_get_args(3, "New domain name is required", cmd)?;
     let new_vol_path = clone_get_args(4, "New volume path is required", cmd)?;
 
-    let new_pool = StoragePool::lookup_by_name(&conn, &settings.get_string("POOL")?)?;
-
     clone_domain(
         &conn,
-        &dom.get_name()?,
-        &dom.get_xml_desc(0)?,
-        &get_volume_from_domain(&dom)?,
+        &get_domain(&conn, cmd)?,
         &new_name,
         &new_pool,
         &new_vol_path,
